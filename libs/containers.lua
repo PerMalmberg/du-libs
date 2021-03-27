333885b3d0cd3dc1e4b61cc955c665b4e869b29b
Per Malmberg <per.malmberg@gmail.com>
2021-03-27 10:18:17 +0100

    Add skill level support.

--- libs/containers.lua
+++ libs/containers.lua
@@ -1,27 +1,60 @@
 require("link_elements")
+require("skill_levels")
 
-function getContainerMassFromHitpoints(containerId)
+function getContainerDataFromHitpoints(containerId)
     local maxHitPoints = linkedCore.getElementMaxHitPointsById(containerId)
+
+    local o = {}
 
     if maxHitPoints >= 69267 then
         -- XXL
-        return 884013
+        o.mass = 884013
+        o.capacity = adjustContainerVolume(512000)
     elseif maxHitPoints >= 34633 then
         -- XL
-        return 44206
+        o.mass = 44206
+        o.capacity = adjustContainerVolume(256000)
     elseif maxHitPoints >= 17316 then
         -- L
-        return 14842.7
+        o.mass = 14842.7
+        o.capacity = adjustContainerVolume(128000)
     elseif maxHitPoints >= 7997 then
         -- M
-        return 7421.35
+        o.mass = 7421.35
+        o.capacity = adjustContainerVolume(64000)
     elseif maxHitPoints >= 999 then
         -- S
-        return 1281.31
+        o.mass = 1281.31
+        o.capacity = adjustContainerVolume(8000)
     else
         -- xs
-        return 229.09
+        o.mass = 229.09
+        o.capacity = adjustContainerVolume(1000)
     end
+
+    return o
+end
+
+function calculateActualMass(mass)
+    local lvl = getContainerOptimizationLevel()
+
+    if lvl > 0 then
+       mass = mass / (1 - lvl * 0.05)
+    end
+
+    return mass
+end
+
+function adjustContainerVolume(volume)
+    local perLevel = 0.1
+
+    local lvl = getContainerProficiencyLevel()
+
+    if lvl > 0 then
+        volume = volume * (1 + lvl * perLevel)
+    end
+
+    return volume
 end
 
 function get_containers(byName)
@@ -35,11 +68,18 @@ function get_containers(byName)
     for _, id in pairs(element_ids) do
         if linkedCore.getElementTypeById(id) == "Container" then
       
+            -- The mass we get here is adjusted for skills applied to the container.
+            local contData = getContainerDataFromHitpoints(id)
+
+            local reducedMass = linkedCore.getElementMassById(id) - contData.mass
+
             local container_info = {
-                "id", "name", "content_mass",
+                "id", "name", "content_mass", "container_volume", "capacity",
                 id = id,
                 name = linkedCore.getElementNameById(id),
-                content_mass = linkedCore.getElementMassById(id) - getContainerMassFromHitpoints(id)
+                content_reduced_mass = reducedMass,
+                content_actual_mass = calculateActualMass(reducedMass),
+                capacity = contData.capacity
             }
 
             if byContainerName then
---
+++ libs/skill_levels.lua
@@ -0,0 +1,10 @@
+local containerOptimizationLevel = 0 --export: Container Optimization Level
+local containerProficiencyLevel = 0 --export: Container Proficiency Level
+
+function getContainerOptimizationLevel()
+    return containerOptimizationLevel
+end
+
+function getContainerProficiencyLevel()
+    return containerProficiencyLevel
+end
