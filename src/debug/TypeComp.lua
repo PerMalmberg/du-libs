435fa0e2bcd0fdb4716b438f1613450b230c7599
Per Malmberg <per.malmberg@gmail.com>
2022-06-12 11:23:11 +0200

    Fix capitalisation

--- src/debug/TypeComp.lua
+++ src/debug/TypeComp.lua
@@ -21,7 +21,7 @@ function typeComp.IsBoolean(b)
     return type(b) == "boolean"
 end
 
-function typeComp.isVec3(v)
+function typeComp.IsVec3(v)
     return typeComp.IsTable(v) and typeComp.IsNumber(v.x and v.y and v.z) and typeComp.IsFunction(v.trim_inplace)
 end
 
