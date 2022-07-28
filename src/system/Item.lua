59bda9e378d01134321beae9cd0f6c375df014c0
Per Malmberg <per.malmberg@gmail.com>
2022-07-28 15:50:46 +0200

    Add Item.lua

---
+++ src/system/Item.lua
@@ -0,0 +1,40 @@
+local Item = {}
+Item.__index = Item
+function Item:New(core)
+    local self = {}
+
+    -- Returns the list of items on the construct which has all the given name parts.
+    function self:FilterItemsByName(...)
+        local items = {}
+        for _, lid in ipairs(core.getElementIdList()) do
+            local info = system.getItem(core.getElementItemIdById(lid))
+            local l = info.name:lower()
+            local terms = { ... }
+            local count = 0
+
+            for _, part in ipairs(terms) do
+                if string.find(l, part, 1, true) then
+                    count = count + 1
+                else
+                    break
+                end
+            end
+
+            if count == #terms then
+                table.insert(items, info)
+                system.print(info.name)
+            end
+        end
+
+        return items
+    end
+
+    return setmetatable(self, Item)
+end
+
+-- Static function
+Item.staticFunction = function()
+
+end
+
+return Item
