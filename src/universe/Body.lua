8b4fa94ae630c05f7a17f1d8e490ff167ad3a099
Per Malmberg <per.malmberg@gmail.com>
2022-10-25 21:12:02 +0200

    Add AboveSeaLevel.

--- src/universe/Body.lua
+++ src/universe/Body.lua
@@ -3,6 +3,7 @@
 require("system/locale")
 local checks = require("debug/Checks")
 local Vec3 = require("cpml/vec3")
+local abs = math.abs
 
 local max = math.max
 
@@ -13,14 +14,15 @@ local max = math.max
 ---@field Id number The body ID
 ---@field Name string The name of the body
 ---@field Type string The type of the body
----@field Physics table Physics properties
----@field Geography table Geography properties
----@field Atmosphere table Atmosphere properties
+---@field Physics { Gravity:number } Physics properties
+---@field Geography { Center:vec3, Radius:number, MaxSurfaceAltitude:number} Geography properties
+---@field Atmosphere {Present:boolean, Thickness:number, Radius:number} Atmosphere properties
 ---@field Surface table Surface properties
 ---@field PvP table Pvp properties
 ---@field DistanceToAtmo fun(self:Body, point:Vec3):number
 ---@field IsInAtmo fun(self:Body, point:Vec3):boolean
 ---@field DistanceToHighestPossibleSurface fun(coordinate:Vec3)
+---@field AboveSeaLevel fun(coordinate:Vec3):boolean, number
 
 local Body = {}
 Body.__index = Body
@@ -31,7 +33,7 @@ function Body.New(galaxy, bodyData)
 
     local language = LocaleIndex()
 
-    local s = {
+    local s = { ---@type Body
         Galaxy = galaxy,
         Id = bodyData.id,
         Name = bodyData.name[language],
@@ -41,8 +43,7 @@ function Body.New(galaxy, bodyData)
         },
         Geography = {
             Center = Vec3(bodyData.center),
-            Radius = bodyData.radius,
-            MaxSurfaceAltitude = bodyData.surfaceMaxAltitude
+            Radius = bodyData.radius -- This is the water level, i.e. 0 elevation
         },
         Atmosphere = {
             Present = bodyData.hasAtmosphere,
@@ -76,10 +77,19 @@ function Body.New(galaxy, bodyData)
         return s:DistanceToAtmo(coordinate) == 0
     end
 
-    ---Returns the distance to the highest possible point on the body or 0 if below lowest surface
+    ---Returns the distance to the highest possible point on the body or 0 if below higest surface
     ---@param coordinate Vec3
     function s.DistanceToHighestPossibleSurface(coordinate)
-        return max(0, (coordinate - s.Geography.Center):len() - s.Geography.MaxSurfaceAltitude)
+        return max(0, (coordinate - s.Geography.Center):len() - s.Geography.Radius - s.Surface.MaxSurfaceAltitude)
+    end
+
+    ---Returns a boolean indicating if the coordinate is above sea level and a number representing the absolute distance to the sea level
+    ---@param coordinate vec3 The coordinate to get info on.
+    ---@return boolean, number
+    function s.AboveSeaLevel(coordinate)
+        local seaLevel = s.Geography.Radius
+        local distanceToCenter = (coordinate - s.Geography.Center):len()
+        return distanceToCenter >= seaLevel, abs(distanceToCenter - seaLevel)
     end
 
     return setmetatable(s, Body)
