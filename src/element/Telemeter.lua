3c90c8deffb413e3f4f6c5d20992a165014b39a2
Per Malmberg <per.malmberg@gmail.com>
2023-01-06 09:04:42 +0100

    Add Telemeter.

---
+++ src/element/Telemeter.lua
@@ -0,0 +1,39 @@
+local Vec3 = require("math/Vec3")
+
+---@alias TelemeterVec3 {x:number, y:number, z:number}
+---@alias TelemeterData {hit:boolean, distance:number, point:TelemeterVec3}
+---@alias TeleRaycast fun():TelemeterData
+---@alias TeleDist fun():number
+---@alias TeleWorldAxis fun():TelemeterVec3
+---@alias TelementerAPI {raycast:TeleRaycast, getMaxDistance:TeleDist, getRayWorldAxis:TeleWorldAxis, getRayWorldOrigin:TeleWorldAxis}
+---@alias TelemeterResult {Hit:boolean, distance:number, point:Vec3}
+
+---@class Telemeter
+
+local Telemeter = {}
+Telemeter.__index = Telemeter
+
+---Create a new Telemeter
+---@param telemeter TelementerAPI
+function Telemeter.New(telemeter)
+    local s = {}
+
+    ---Measure distance
+    ---@return TelemeterResult
+    function s.Measure()
+        local res = telemeter.raycast()
+        return { Hit = true, Point = Vec3.New(res.point), Distance = res.distance }
+    end
+
+    ---Checks if the link has the expected functions
+    ---@return boolean
+    function s.IsTelemeter()
+        return type(telemeter.getMaxDistance) == "function"
+            and type(telemeter.raycast) == "function"
+            and type(telemeter.getRayWorldOrigin) == "function"
+    end
+
+    return setmetatable(s, Telemeter)
+end
+
+return Telemeter
